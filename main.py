c51c6138ae0e95d20db28492062f3320f1a346c4
Connor Davis <37542791+davisjc22@users.noreply.github.com>
2022-03-20 19:20:07 -0500

    Change test size and manually calculate accuracy

--- main.py
+++ main.py
@@ -6,6 +6,39 @@ import matplotlib.pyplot as plt
 #  import shap
 
 
+def R2(y, y_hat):
+    # print("y: ", y, ", y_hat: ", y_hat)
+    ss_res = K.sum(K.square(y - y_hat))
+    ss_tot = K.sum(K.square(y - K.mean(y)))
+    answer = ( 1 - ss_res/(ss_tot + K.epsilon()) )
+    # print("R2 type: ", answer.dtype)
+    return answer
+
+
+def my_loss_fn(y_true, y_pred):
+    # print("y_true: ", y_true, ", y_pred: ", y_pred)
+    squared_difference = K.square(y_true - y_pred)
+    answer = tf.reduce_mean(squared_difference, axis=-1)  # Note the `axis=-1`
+    # print("loss type: ", answer.dtype)
+    return answer
+
+
+def my_metric_fn(y_true, y_pred):
+    # print("y_true: ", y_true, ", y_pred: ", y_pred)
+    squared_difference = tf.square(y_true - y_pred)
+    answer = tf.reduce_mean(squared_difference, axis=-1)  # Note the `axis=-1`
+    return answer
+
+
+def get_bid(bid_val):
+    bid_num = bid_val*50 + 70
+    return bid_num
+
+
+model_key = ['R5', 'R6', 'R7', 'R8', 'R9', 'R10', 'R11', 'R12', 'R13', 'R14',
+             'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'B11', 'B12', 'B13', 'B14',
+             'G5', 'G6', 'G7', 'G8', 'G9', 'G10', 'G11', 'G12', 'G13', 'G14',
+             'Y5', 'Y6', 'Y7', 'Y8', 'Y9', 'Y10', 'Y11', 'Y12', 'Y13', 'Y14', 'Rook']
 
 # Read In Data
 file = open('raven.csv')
@@ -57,20 +90,23 @@ for row in csvreader:
                     hand = i + j + k + l + rook  # combine the 4 colors and rook into one list
                     hands.append(hand)
                     bids.append(bid)
-                    print(hand, " ", bid)
+                    # print(hand, " ", bid)
 
 
 # 500 original hands * 16 permutations for each hand = 8000 hands
 # use 87.5% of hands for training (7000) and 12.5% for testing (1000)
 train_bids = bids
-train_bids = [int(x) for x in train_bids]
-test_hands = hands[7000:]
-test_bids = train_bids[7000:]
-train_hands = hands[:7000]
-train_bids = train_bids[:7000]
+# train_bids = [int(x) for x in train_bids]
+test_hands = hands[9600:]
+test_bids = train_bids[9600:]
+train_hands = hands[:9600]
+train_bids = train_bids[:9600]
+# print(len(hands))
+# print(len(bids))
+# print(len(test_hands))
+# print(len(test_bids))
 
 
-#print(len(train_bids))
 n_features = 41
 model = models.Sequential(name="DeepNN", layers=[
     ### hidden layer 1
@@ -94,21 +130,15 @@ model = models.Sequential(name="DeepNN", layers=[
 ])
 model.summary()
 
-# define metrics
-def R2(y, y_hat):
-    ss_res = K.sum(K.square(y - y_hat))
-    ss_tot = K.sum(K.square(y - K.mean(y)))
-    return ( 1 - ss_res/(ss_tot + K.epsilon()) )
-
-
 # compile the neural network
-model.compile(optimizer='adam', loss='mean_absolute_error', metrics=[R2])
+model.compile(optimizer='adam', loss="mean_absolute_error", metrics=[R2])
 
 # train/validation
-training = model.fit(x=train_hands, y=train_bids, batch_size=32, epochs=100, shuffle=True, verbose=0,
+training = model.fit(x=train_hands, y=train_bids, batch_size=32, epochs=100, shuffle=False, verbose=0,
                      validation_data=(test_hands, test_bids))
+#training = model.fit(x=train_hands, y=train_bids, batch_size=32, epochs=100, shuffle=True, verbose=0)
 
-# plot
+## plot
 metrics = [k for k in training.history.keys() if ("loss" not in k) and ("val" not in k)]
 fig, ax = plt.subplots(nrows=1, ncols=2, sharey=True, figsize=(15, 3))
 
@@ -122,7 +152,6 @@ for metric in metrics:
     ax11.plot(training.history[metric], label=metric)
     ax11.set_ylabel("Score", color='steelblue')
 ax11.legend()
-print("finished training")
 
 ## validation
 ax[1].set(title="Validation")
@@ -135,4 +164,25 @@ for metric in metrics:
     ax22.set_ylabel("Score", color="steelblue")
 plt.show()
 
-model.save('trained_model/bidding_model')
+# model.save('trained_model/bidding_model')
+
+num_correct = 0
+# loop through test bids and manually print and calculate accuracy
+for i in range(len(test_hands)):
+    answer = model.predict([test_hands[i]])
+    # print(answer)
+    answer = round(answer[0][0], 1)
+    # print("prediction: ", answer, "real: ", test_bids[i])
+    hand_str = ""
+    for j in range(len(test_hands[i])):
+        if test_hands[i][j] is 1:
+            hand_str = hand_str + " " + model_key[j]
+    guess_bid = round(get_bid(answer), 0)
+    correct_bid = get_bid(test_bids[i])
+    print(hand_str, " guess bid: ", guess_bid, "| correct bid: ", correct_bid)
+    if abs(guess_bid - correct_bid) <= 5.0:
+        num_correct = num_correct + 1
+accuracy = num_correct / len(test_hands)
+print("Test Accuracy: ", accuracy)
+    # do some math to allow a range of inaccuracy
+    # compute overall accuracy
